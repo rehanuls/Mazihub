0aa431c6780e87a3b4c6d0795aa0a6649e71ca70
rehanuls <58020963+rehanuls@users.noreply.github.com>
2025-12-18 01:01:17 +0700

    Add autocrateopen module

--- scripts/MWB/Connector/init.lua
+++ scripts/MWB/Connector/init.lua
@@ -1,8 +1,19 @@
 local Autocollect = loadstring(game:HttpGet(
 	"https://raw.githubusercontent.com/rehanuls/Mazihub/main/scripts/MWB/functions/autocollect.lua"
 ))()
+local AutoOpenCrate = loadstring(game:HttpGet(
+	"https://raw.githubusercontent.com/rehanuls/Mazihub/main/scripts/MWB/functions/autoopencrate.lua"
+))()
 
 local Controller = {}
+
+function Controller.toggleAutoOpen(state)
+	if state then
+		AutoOpenCrate.start()
+	else
+		AutoOpenCrate.stop()
+	end
+end
 
 function Controller.toggleAutoCollect(state, delay)
 	if state then
